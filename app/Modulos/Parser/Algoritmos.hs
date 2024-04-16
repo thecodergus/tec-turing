4d25bb1e1bd6b6e8eaeeddf9d9658d56323f32f4
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-04-16 18:44:41 -0300

    Comentando o codigo

--- app/Modulos/Parser/Algoritmos.hs
+++ app/Modulos/Parser/Algoritmos.hs
@@ -13,18 +13,23 @@ import Text.Parsec
       many )
 import Text.Parsec.String (Parser)
 
+-- | O parser 'symbolParser' reconhece um caractere que não seja um dos seguintes: ";", "*", "_", "\n", "\r" ou "\t".
 symbolParser :: Parser Char
 symbolParser = noneOf ";*_\n\r\t "
 
+-- | O parser 'stateParser' reconhece uma sequência de caracteres alfanuméricos ou o caractere "*".
 stateParser :: Parser String
 stateParser = many1 (alphaNum <|> char '*')
 
+-- | O parser 'directionParser' reconhece um dos seguintes caracteres: "l", "r", "*" ou "r".
 directionParser :: Parser Char
 directionParser = oneOf "lrr*"
 
+-- | O parser 'breakpointParser' reconhece o caractere "!".
 breakpointParser :: Parser Char
 breakpointParser = char '!'
 
+-- | O parser 'turingTupleParser' reconhece uma tupla de transição da Máquina de Turing.
 turingTupleParser :: Parser TuringTuple
 turingTupleParser = do
     currentState <- stateParser
@@ -40,5 +45,6 @@ turingTupleParser = do
 
     return $ TuringTuple currentState currentSymbol newSymbol direction newState
 
+-- | O parser 'turingProgramParser' reconhece uma lista de tuplas de transição da Máquina de Turing, separadas por quebras de linha.
 turingProgramParser :: Parser [TuringTuple]
 turingProgramParser = many (turingTupleParser <* endOfLine)
--- app/Modulos/Parser/Tipos.hs
+++ app/Modulos/Parser/Tipos.hs
@@ -1,11 +1,12 @@
 module Parser.Tipos where
 
 
+-- | Definição do tipo de dado 'TuringTuple' que representa uma tupla de transição em uma máquina de Turing.
 data TuringTuple = TuringTuple
-  { currentState :: String,
-    currentSymbol :: Char,
-    newSymbol :: Char,
-    direction :: Char,
-    newState :: String
+  { currentState :: String,    -- ^ O estado atual da máquina de Turing.
+    currentSymbol :: Char,     -- ^ O símbolo atual lido pela máquina de Turing.
+    newSymbol :: Char,         -- ^ O novo símbolo a ser escrito pela máquina de Turing.
+    direction :: Char,         -- ^ A direção em que a cabeça de leitura/escrita da máquina de Turing deve se mover.
+    newState :: String          -- ^ O novo estado em que a máquina de Turing deve entrar.
   }
   deriving (Show)
