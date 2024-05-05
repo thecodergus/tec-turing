6084e8bb37953d445eda6ef9461ba1c40ee330fb
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-05-05 14:27:55 -0300

    Ajeitando transformação dos dados

--- app/Main.hs
+++ app/Main.hs
@@ -5,14 +5,11 @@ import Utils (instrucoesParaMaquinaTuring)
 
 main :: IO ()
 main = do
-    putStrLn "Hello, Haskell!"-- | O parser 'turingProgramParser' reconhece uma lista de tuplas de transição da Máquina de Turing, separadas por quebras de linha.
     input <- readFile "teste.in"
-    print "Input:"
-    print input
-    print "Input limpo:"
-    print $ removerComentarios input
-    print "Instrucoes:"
     let retorno = parser input
 
-    print $ fst retorno
-    print $ snd retorno
+    case snd retorno of
+        Left erro -> print erro
+        Right instrucoes -> do
+            let maquina = instrucoesParaMaquinaTuring (fst retorno, instrucoes)
+            print maquina
