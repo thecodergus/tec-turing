aaa9b14ee27118394b85865a586d257788071284
PCdaFacul <gustavo.michels.de.camargo@gmail.com>
2024-05-22 12:30:16 -0300

    Salvando o que fiz até aqui

--- app/Main.hs
+++ app/Main.hs
@@ -5,16 +5,17 @@ import Utils (instrucoesParaMaquinaTuring)
 import Algoritmos.Sipser (conveterParaDuplamenteInfinita)
 import Algoritmos (salvarArquivo)
 import Tipos (transicoes)
+import Algoritmos.DuplamenteInfinita (converterParaSipser)
 
 main :: IO ()
 main = do
-    input <- readFile "/home/udesc/Documentos/tec-turing/teste_sipser.txt"
+    input <- readFile "/home/udesc/Documentos/tec-turing/teste_duplamente_infinita.txt"
     let retorno = parser input
 
     case snd retorno of
         Left erro -> print erro
         Right instrucoes -> do
             let maquina = instrucoesParaMaquinaTuring (fst retorno, instrucoes)
-            let maquina' = conveterParaDuplamenteInfinita maquina
+            let maquina' = converterParaSipser maquina
             _ <- salvarArquivo maquina' "/home/udesc/Documentos/tec-turing/output.out"
             print maquina
--- app/Modulos/Algoritmos/DuplamenteInfinita.hs
+++ app/Modulos/Algoritmos/DuplamenteInfinita.hs
@@ -1,5 +1,5 @@
 module Algoritmos.DuplamenteInfinita where
-import qualified Tipos
+import Tipos
 
 
 -- converterParaSipser :: Tipos.MaquinaTuring -> Tipos.MaquinaTuring
@@ -23,3 +23,47 @@ import qualified Tipos
         --                                 passoUm :: Tipos.Simbolo -> Tipos.Transicao
         --                                 passoUm Tipos.Vazio = Tipos.Transicao (Tipos.Estado "0") (Tipos.SimboloAtual $ Tipos.Simbolo "_") (Tipos.SimboloSerEscrito $ Tipos.Simbolo "#") Tipos.Direita (Tipos.Estado ("hashtagInicial" ++ "_"))
         --                                 passoUm (Tipos.Simbolo sim) = Tipos.Transicao (Tipos.Estado "0") (Tipos.SimboloAtual $ Tipos.Simbolo sim) (Tipos.SimboloSerEscrito $ Tipos.Simbolo "#") Tipos.Direita (Tipos.Estado ("hashtagInicial" ++ sim))
+
+converterParaSipser :: MaquinaTuring -> MaquinaTuring
+converterParaSipser (MaquinaTuring _ _ _ _ Sipser) = error "A Maquina de Turing já é do tipo fita Sipser"
+converterParaSipser (MaquinaTuring estados' alfabeto' transicoes' estadoInicial' _) = MaquinaTuring estados' alfabeto' (passoQuatro $ passoUm transicoes' ++ passoDois alfabeto' ++ passoTres alfabeto') estadoInicial' Sipser
+        where
+        -- Substituir estado "0" por estado inicio
+        passoUm :: [Transicao] -> [Transicao]
+        passoUm [] = []
+        passoUm ((Transicao (Estado "0") simbAtual simbSerEscrito direcao (Estado "0")) : xs) = Transicao (Estado "inicio") simbAtual simbSerEscrito direcao (Estado "inicio") : passoUm xs
+        passoUm ((Transicao (Estado "0") simbAtual simbSerEscrito direcao paraEstado) : xs) = Transicao (Estado "inicio") simbAtual simbSerEscrito direcao paraEstado : passoUm xs
+        passoUm ((Transicao estadoAtual' simbAtual simbSerEscrito direcao (Estado "0")) : xs) = Transicao estadoAtual' simbAtual simbSerEscrito direcao (Estado "inicio") : passoUm xs
+        passoUm ((Transicao estadoAtual' simbAtual simbSerEscrito direcao paraEstado) : xs) = Transicao estadoAtual' simbAtual simbSerEscrito direcao paraEstado : passoUm xs
+
+        -- Adicionar meu estado inicial "0", o que inclui o uso de # e mover os itens para a direita
+        passoDois :: [Simbolo] -> [Transicao]
+        passoDois alfabeto'' = estado0 alfabeto'' ++ estadosMover alfabeto'' alfabeto''
+                where
+                estado0 :: [Simbolo] -> [Transicao]
+                estado0 = map aux
+                        where
+                                aux :: Simbolo -> Transicao
+                                aux Vazio = Transicao (Estado "0") (SimboloAtual Vazio) (SimboloSerEscrito (Simbolo "#")) Direita (Estado "mover-direita-{}")
+                                aux s@(Simbolo simb) = Transicao (Estado "0") (SimboloAtual s) (SimboloSerEscrito (Simbolo "#")) Direita (Estado ("mover-direita-{" ++ simb ++ "}"))
+
+                -- mover-x :: {a,b,c,d...}, x, r, mover-{a,b,c,d...}
+                estadosMover :: [Simbolo] -> [Simbolo] -> [Transicao]
+                estadosMover [] _ = []
+                estadosMover _ [] = []
+                estadosMover (a : as) alfabetoAux = map (a `aux`) alfabetoAux ++ estadosMover as alfabetoAux
+                        where
+                                aux :: Simbolo -> Simbolo -> Transicao
+                                aux Vazio Vazio = Transicao (Estado "mover-direita-vazio") (SimboloAtual Vazio) (SimboloSerEscrito Vazio) Esquerda (Estado "retorno-{}")
+                                aux Vazio (Simbolo simb) = Transicao (Estado "mover-direita-vazio") (SimboloAtual (Simbolo simb)) (SimboloSerEscrito (Simbolo "_")) Direita (Estado ("mover-direita-{" ++ simb ++ "}"))
+                                aux (Simbolo x) (Simbolo simb) = Transicao (Estado ("mover-direita-{" ++ x ++ "}")) (SimboloAtual (Simbolo simb)) (SimboloSerEscrito (Simbolo x)) Direita (Estado ("mover-direita-{" ++ simb ++ "}"))
+                                aux (Simbolo x) Vazio = Transicao (Estado ("mover-direita-{" ++ x ++ "}")) (SimboloAtual Vazio) (SimboloSerEscrito (Simbolo x)) Direita (Estado "mover-direita-{}")
+
+        -- Apos encontrar branco duas vezes, voltar para a esquerda até achar o #
+        passoTres :: [Simbolo] -> [Transicao]
+        passoTres alfabeto'' = map (\(Simbolo s) -> Transicao (Estado ("mover-direita-{" ++ s ++ "}")) (SimboloAtual Vazio) (SimboloSerEscrito (Simbolo s)) Esquerda (Estado "retornar-hashtag")) alfabeto'' ++ [Transicao (Estado "retornar-hashtag") (SimboloAtual (Simbolo "#")) Manter Direita (Estado "inicio"), Transicao (Estado "retornar-hashtag") Todos Manter Esquerda (Estado "retornar-hashtag")]
+
+        passoQuatro :: [Transicao] -> [Transicao]
+        passoQuatro [] = []
+        passoQuatro ((Transicao estadoAtual' simboloAtual' simboloSerEscrito' Esquerda paraEstado') : estados'') = 
+        passoQuatro (t : ts) = passoQuatro ts
