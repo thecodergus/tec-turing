a5f17ad5ecbe7b22ca797802279fd19c23aaff1c
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-05-26 21:41:46 -0300

    Sipser refeito

--- app/Modulos/Algoritmos/Sipser.hs
+++ app/Modulos/Algoritmos/Sipser.hs
@@ -1,56 +1,80 @@
 module Algoritmos.Sipser where
 import Tipos
-    ( Direcao(Direita, Esquerda, Parado),
-      SimboloSerEscrito(SimboloSerEscrito, Manter),
-      SimboloAtual(SimboloAtual, Todos),
-      Tipo(DuplamenteInfinita, Sipser),
-      Transicao(Transicao, estadoAtual),
-      Simbolo(..),
+    ( Direcao(Esquerda, Direita),
       Estado(Estado),
-      MaquinaTuring(MaquinaTuring, transicoes) )
+      MaquinaTuring(MaquinaTuring),
+      Simbolo(..),
+      SimboloAtual(Todos, SimboloAtual),
+      SimboloSerEscrito(Manter, SimboloSerEscrito),
+      Tipo(DuplamenteInfinita, Sipser),
+      Transicao(Transicao, estadoAtual, proximoEstado) )
 import Data.List (nub)
 
 
 conveterParaDuplamenteInfinita :: MaquinaTuring -> MaquinaTuring
 conveterParaDuplamenteInfinita (MaquinaTuring _ _ _ _ DuplamenteInfinita) = error "A Maquina de Turing já é do tipo fita duplamente infinita"
-conveterParaDuplamenteInfinita (MaquinaTuring estados alfabeto transicoes' estadoInicial Sipser) = MaquinaTuring estados alfabeto (passoUm transicoes' ++ passoDois alfabeto ++ passoTres alfabeto ++ passoQuatro estados) estadoInicial DuplamenteInfinita
+conveterParaDuplamenteInfinita (MaquinaTuring estados alfabeto transicoes' estadoInicial Sipser) = 
+    MaquinaTuring 
+    estados 
+    alfabeto 
+    (
+        passoUm transicoes'
+        ++ passoDois alfabeto
+        ++ passoTres alfabeto (alfabeto ++ [Vazio])
+        ++ passoQuatro
+        ++ passoCinco (alfabeto ++ [Vazio])
+        ++ passoSeis estados
+    ) 
+    estadoInicial 
+    DuplamenteInfinita
     where
-        -- Substituir estado "0" por estado inicio
-        passoUm :: [Transicao] -> [Transicao]
-        passoUm [] = []
-        passoUm ((Transicao (Estado "0") simbAtual simbSerEscrito direcao (Estado "0")) : xs) = Transicao (Estado "inicio-s") simbAtual simbSerEscrito direcao (Estado "inicio-s") : passoUm xs
-        passoUm ((Transicao (Estado "0") simbAtual simbSerEscrito direcao paraEstado) : xs) = Transicao (Estado "inicio-s") simbAtual simbSerEscrito direcao paraEstado : passoUm xs
-        passoUm ((Transicao estadoAtual' simbAtual simbSerEscrito direcao (Estado "0")) : xs) = Transicao estadoAtual' simbAtual simbSerEscrito direcao (Estado "inicio-s") : passoUm xs
-        passoUm ((Transicao estadoAtual' simbAtual simbSerEscrito direcao paraEstado) : xs) = Transicao estadoAtual' simbAtual simbSerEscrito direcao paraEstado : passoUm xs
+    -- Substituir "0" por inicio
+    passoUm :: [Transicao] -> [Transicao]
+    passoUm [] = []
+    passoUm (t@(Transicao (Estado "0") _ _ _ (Estado "0")) : ts) = t {estadoAtual = Estado "inicio", proximoEstado = Estado "inicio"} : passoUm ts
+    passoUm (t@(Transicao (Estado "0") _ _ _ _) : ts) = t {estadoAtual = Estado "inicio"} : passoUm ts
+    passoUm (t@(Transicao _ _ _ _ (Estado "0")) : ts) = t {proximoEstado = Estado "inicio"} : passoUm ts
+    passoUm (t : ts) = t : passoUm ts
 
-        -- Adicionar meu estado inicial "0", o que inclui o uso de # e mover os itens para a direita
-        passoDois :: [Simbolo] -> [Transicao]
-        passoDois alfabeto' = estado0 alfabeto' ++ estadosMover alfabeto' alfabeto'
-            where
-                estado0 :: [Simbolo] -> [Transicao]
-                estado0 = map aux
-                    where
-                        aux :: Simbolo -> Transicao
-                        aux Vazio = Transicao (Estado "0") (SimboloAtual Vazio) (SimboloSerEscrito (Simbolo "#")) Direita (Estado "mover-direita-vazio")
-                        aux s@(Simbolo simb) = Transicao (Estado "0") (SimboloAtual s) (SimboloSerEscrito (Simbolo "#")) Direita (Estado ("mover-direita-" ++ simb))
+    -- Adicionando nova configuração inicial
+    passoDois :: [Simbolo] -> [Transicao]
+    passoDois [] = []
+    passoDois (Vazio : ss) = Transicao (Estado "0") (SimboloAtual Vazio) (SimboloSerEscrito (Simbolo "#")) Direita (Estado "mover-direita-{}") : passoDois ss
+    passoDois (s@(Simbolo simb) : ss) = Transicao (Estado "0") (SimboloAtual s) (SimboloSerEscrito (Simbolo "#")) Direita (Estado ("mover-direita-{" ++ simb ++ "}")) : passoDois ss
 
-                -- mover-x :: {a,b,c,d...}, x, r, mover-{a,b,c,d...}
-                estadosMover :: [Simbolo] -> [Simbolo] -> [Transicao]
-                estadosMover [] _ = []
-                estadosMover _ [] = []
-                estadosMover (a : as) alfabetoAux = map (a `aux`) alfabetoAux ++ estadosMover as alfabetoAux
-                    where
-                        aux :: Simbolo -> Simbolo -> Transicao
-                        aux Vazio Vazio = Transicao (Estado "mover-direita-vazio") (SimboloAtual Vazio) (SimboloSerEscrito Vazio) Esquerda (Estado "retorno-vazio") 
-                        aux Vazio (Simbolo simb) = Transicao (Estado "mover-direita-vazio") (SimboloAtual (Simbolo simb)) (SimboloSerEscrito (Simbolo "_")) Direita (Estado ("mover-direita-" ++ simb))
-                        aux (Simbolo x) (Simbolo simb) = Transicao (Estado ("mover-direita-" ++ x)) (SimboloAtual (Simbolo simb)) (SimboloSerEscrito (Simbolo x)) Direita (Estado ("mover-direita-" ++ simb))
-                        aux (Simbolo x) Vazio = Transicao (Estado ("mover-direita-" ++ x)) (SimboloAtual Vazio) (SimboloSerEscrito (Simbolo x)) Direita (Estado "mover-direita-vazio")
+    -- Adicionandos os demais movimentos a direita "mover-direita-{a,b,c,d...}"
+    passoTres :: [Simbolo] -> [Simbolo] -> [Transicao]
+    passoTres [] _ = []
+    passoTres _ [] = []
+    passoTres (s : ss) alfabeto'' =
+        map (s `passoTres'`) alfabeto''
+            ++ passoTres ss alfabeto''
+        where
+            passoTres' :: Simbolo -> Simbolo -> Transicao
+            passoTres' Vazio Vazio = Transicao (Estado "mover-direita-{}") (SimboloAtual Vazio) (SimboloSerEscrito Vazio) Esquerda (Estado "retornar-quase-vazio")
+            passoTres' Vazio (Simbolo simb) = Transicao (Estado "mover-direita-{}") (SimboloAtual (Simbolo simb)) (SimboloSerEscrito (Simbolo "_")) Direita (Estado ("mover-direita-{" ++ simb ++ "}"))
+            passoTres' (Simbolo x) (Simbolo simb) = Transicao (Estado ("mover-direita-{" ++ x ++ "}")) (SimboloAtual (Simbolo simb)) (SimboloSerEscrito (Simbolo x)) Direita (Estado ("mover-direita-{" ++ simb ++ "}"))
+            passoTres' (Simbolo x) Vazio = Transicao (Estado ("mover-direita-{" ++ x ++ "}")) (SimboloAtual Vazio) (SimboloSerEscrito (Simbolo x)) Direita (Estado "mover-direita-{}")
 
-        -- Apos encontrar branco duas vezes, voltar para a esquerda até achar o #
-        passoTres :: [Simbolo] -> [Transicao]
-        passoTres alfabeto'' = map (\(Simbolo s) -> Transicao (Estado ("mover-direita-" ++ s)) (SimboloAtual Vazio) (SimboloSerEscrito (Simbolo s)) Esquerda (Estado "retornar-hashtag")) alfabeto'' ++ [Transicao (Estado "retornar-hashtag") (SimboloAtual (Simbolo "#")) Manter Direita (Estado "inicio-s"), Transicao (Estado "retornar-hashtag") Todos Manter Esquerda (Estado "retornar-hashtag")]
+    -- Adicionando retorno para o inicio a partir do segundo vazio encontrado
+    passoQuatro :: [Transicao]
+    passoQuatro = [ 
+            Transicao (Estado "mover-direita-{}") (SimboloAtual Vazio) Manter Direita (Estado "retornar-quase-vazio"),
+            Transicao (Estado "retornar-vazio") (SimboloAtual Vazio) Manter Direita (Estado "retornar-comeco"),
+            Transicao (Estado "retornar-comeco") (SimboloAtual (Simbolo "#")) Manter Direita (Estado "inicio"),
+            Transicao (Estado "retornar-comeco") Todos Manter Esquerda (Estado "retornar-comeco")
+        ]
 
+    -- Adicinando transição que decide se é final de entrada ou não
+    passoCinco :: [Simbolo] -> [Transicao]
+    passoCinco [] = []
+    passoCinco s = map passoCinco' s
+      where
+        passoCinco' :: Simbolo -> Transicao
+        passoCinco' (Simbolo s) = Transicao (Estado "retornar-quase-vazio") (SimboloAtual (Simbolo s)) (SimboloSerEscrito Vazio) Esquerda (Estado ("mover-direita-{" ++ s ++ "}"))
+        passoCinco' Vazio = Transicao (Estado "retornar-quase-vazio") (SimboloAtual Vazio) (SimboloSerEscrito Vazio) Esquerda (Estado "retornar-vazio")
 
-        passoQuatro :: [Estado] -> [Transicao]
-        passoQuatro [] = []
-        passoQuatro ((Estado e) : estados') = Transicao (Estado e) (SimboloAtual (Simbolo "#")) Manter Direita (Estado e) : passoQuatro estados'
+    -- Caso algum dos estados chegue ao #, voltar para o estado anterior
+    passoSeis :: [Estado] -> [Transicao]
+    passoSeis [] = []
+    passoSeis ((Estado e) : estados') = Transicao (Estado e) (SimboloAtual (Simbolo "#")) Manter Direita (Estado e) : passoSeis estados'
